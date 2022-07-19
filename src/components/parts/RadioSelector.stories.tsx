8d01e1e1e7ceec8f449f2b0d8ce0b253f6e0145b
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2022-07-20 07:45:52 +0900

    StoryBook でもuseStateを利用してラジオボタンを変更できるようにした

--- src/components/parts/RadioSelector.stories.tsx
+++ src/components/parts/RadioSelector.stories.tsx
@@ -1,24 +1,17 @@
 import { expect } from '@storybook/jest';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { within } from '@storybook/testing-library';
-import { ComponentProps } from 'react';
+import { ChangeEvent, ComponentProps, useCallback, useState } from 'react';
 import { StoryDecorator } from '../StoryDecorator';
 import { RadioSelector } from './RadioSelector';
 
 type Props = ComponentProps<typeof RadioSelector>;
 
-const getProps = (): Props => ({
-  choices: [
-    { value: 'apple', displayValue: 'りんご' },
-    { value: 'banana', displayValue: 'バナナ' },
-    { value: 'orange', displayValue: 'オレンジ' },
-  ],
-  name: 'test',
-  value: 'banana',
-  onChange: () => {
-    /** */
-  },
-});
+const getChoices = (): Props['choices'] => [
+  { value: 'apple', displayValue: 'りんご' },
+  { value: 'banana', displayValue: 'バナナ' },
+  { value: 'orange', displayValue: 'オレンジ' },
+];
 
 export default {
   title: 'parts/RadioSelector',
@@ -26,12 +19,24 @@ export default {
   decorators: [story => <StoryDecorator Story={story} />],
 } as ComponentMeta<typeof RadioSelector>;
 
-const Template: ComponentStory<typeof RadioSelector> = args => {
-  return <RadioSelector {...args} />;
+const Template: ComponentStory<typeof RadioSelector> = props => {
+  const { value: _value, onChange: _onChange, ..._props } = props;
+  const [value, setValue] = useState(_value);
+  const onChange = useCallback((value: string) => {
+    setValue(value);
+  }, []);
+
+  return (
+    <form>
+      <RadioSelector {..._props} value={value} onChange={onChange} />
+    </form>
+  );
 };
 
 export const Normal = Template.bind({});
 
 Normal.args = {
-  ...getProps(),
+  choices: getChoices(),
+  name: 'test',
+  value: 'orange',
 };
