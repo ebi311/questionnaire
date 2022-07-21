6ee9db244474adccf1ebdeec3303b43666ba986d
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2022-07-21 18:08:05 +0900

    フリー入力欄のコントロールを作成した

--- src/__tests__/components/parts/TextArea.test.tsx
+++ src/__tests__/components/parts/TextArea.test.tsx
@@ -2,49 +2,35 @@
  * @jest-environment jsdom
  */
 /*
+- `label`プロパティの文字列を表示する
 - `value`プロパティで渡された文字列を表示する。
 - テキストボックスを変更した場合、`onChange`プロパティのイベントハンドラを実行する。引数に入力内容を渡す。
+- DaisyUI のスタイルをあてる
 */
-import '@testing-library/jest-dom';
 import { fireEvent, render } from '@testing-library/react';
-import { ChangeEvent, ComponentProps, useCallback, useState } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { TextArea } from '~/components/parts/TextArea';
 
-const TestComponent = (props: { defaultValue: string; label: string }) => {
-  const { label, defaultValue } = props;
-  const [value, setValue] = useState(defaultValue);
+const TestComponent = (props: { label: string; value: string }) => {
+  const [value, setValue] = useState(props.value);
   const onChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value);
   }, []);
-  return <TextArea onChange={onChange} value={value} label={label} />;
+  return <TextArea label={props.label} value={value} onChange={onChange} />;
 };
 
-const textArea = (props: ComponentProps<typeof TestComponent>) =>
+const textArea = (props: Parameters<typeof TestComponent>[0]) =>
   render(<TestComponent {...props} />);
 
-test('show label', () => {
-  const target = textArea({
-    label: 'ラベル',
-    defaultValue: '',
-  });
-  expect(target.getByLabelText('ラベル')).toBeInTheDocument();
+test('show value prop.', () => {
+  const target = textArea({ label: 'ラベル', value: 'test' });
+  target.getByLabelText('ラベル');
 });
 
 test('show value prop.', () => {
   const target = textArea({
     label: 'ラベル',
-    defaultValue: 'prop value',
-  });
-  const textAreaElement = target.getByTestId(
-    'text-area',
-  ) as HTMLTextAreaElement;
-  expect(textAreaElement.value).toBe('prop value');
-});
-
-test('show value prop.', () => {
-  const target = textArea({
-    label: 'ラベル',
-    defaultValue: 'prop value',
+    value: 'prop value',
   });
   const textAreaElement = target.getByTestId(
     'text-area',
@@ -55,7 +41,7 @@ test('show value prop.', () => {
 test('call onChange', () => {
   const target = textArea({
     label: 'ラベル',
-    defaultValue: 'prop value',
+    value: 'prop value',
   });
   const textAreaElement = target.getByTestId(
     'text-area',
--- src/components/parts/TextArea.tsx
+++ src/components/parts/TextArea.tsx
@@ -1,24 +1,23 @@
 import classNames from 'classnames';
-import { TextareaHTMLAttributes, useId } from 'react';
+import React, { TextareaHTMLAttributes, useId } from 'react';
 
 type Props = {
   label: string;
   //
-} & TextareaHTMLAttributes<HTMLTextAreaElement>;
+} & Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'id'>;
 
 export const TextArea = (props: Props) => {
-  const { label, className, ..._props } = props;
   const id = useId();
   return (
     <>
-      <label className="label" htmlFor={id}>
-        <span className="label-text">{label}</span>
+      <label htmlFor={id} className="label">
+        <span className="label-text">{props.label}</span>
       </label>
       <textarea
         id={id}
-        {..._props}
-        className={classNames(className, 'textarea', 'textarea-primary')}
         data-testid="text-area"
+        {...props}
+        className={classNames(props.className, 'textarea textarea-primary')}
       />
     </>
   );
